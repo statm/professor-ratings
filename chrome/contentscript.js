5c08bc207b035d3200ac6e3e2280a6fef648e86f
R Zhao <arcus.zhao@gmail.com>
2014-01-28 14:29:18 -0800

    data: initial commit

--- chrome/contentscript.js
+++
@@ -1,8 +0,0 @@
-$("td.instructor").each(function() {
-    var professorName = $(this).text();
-    if (professorName != ""
-        && rmpData.hasOwnProperty(professorName)) {
-        data = rmpData[professorName];
-        $(this).append(" <a href='http://www.ratemyprofessors.com/ShowRatings.jsp?tid=" + data.id + "' style='background-image: none;' target='_blank'>(" + data.a + ")</a>");
-    }
-});
