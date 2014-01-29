09e211ea01178dfbc05a5976205de4ba16791355
R Zhao <arcus.zhao@gmail.com>
2014-01-28 19:50:42 -0800

    Chrome: handle cases with more than one instructor

--- chrome/contentscript.js
+++ chrome/contentscript.js
@@ -1,25 +1,42 @@
 var SERVER_URL = "http://statm.github.io/professor-ratings/";
 
-var localVersion = localStorage["version"];
-var remoteVersion = $.ajax({url: SERVER_URL + "version", async: false}).responseText;
-
-console.log("local=" + localVersion + ", remote=" + remoteVersion);
-
-if (localVersion === undefined || parseInt(remoteVersion) > parseInt(localVersion)) {
-    console.log("update required");
-    updateData();
-}
-
 var rmpData = JSON.parse(localStorage["data"]);
 
 $("td.instructor").each(function() {
-    var professorName = $(this).text();
-    if (professorName != ""
-        && rmpData.hasOwnProperty(professorName)) {
-        data = rmpData[professorName];
-        $(this).append(" <a href='http://www.ratemyprofessors.com/ShowRatings.jsp?tid=" + data.id + "' style='background-image: none;' target='_blank'>(" + data.a + ")</a>");
-    }
+    $(this).contents().filter(function(){
+        return this.nodeType == 3 || (this.nodeType == 1 && this.nodeName == "A");
+    }).each(function(){
+        var professorName = $(this).text().replace(",", "");
+        if (professorName != ""
+            && rmpData.hasOwnProperty(professorName)) {
+            console.log("hit");
+            entry = rmpData[professorName];
+            if (parseInt(entry.r) > 0) {
+                $(this).after("<a href='http://www.ratemyprofessors.com/ShowRatings.jsp?tid=" + entry.id + "' style='background-image: none;' target='_blank' class='tooltip' title='" + entry.a + "'>(" + entry.a + ")</a>");
+            }
+        }
+    });
 });
+
+$(".tooltip").tooltipster({
+    position: 'left',
+    theme: 'tooltipster-light',
+    interactive: true
+});
+
+//
+
+function checkVersion() {
+    var localVersion = localStorage["version"];
+    var remoteVersion = $.ajax({url: SERVER_URL + "version", async: false}).responseText;
+
+    console.log("local=" + localVersion + ", remote=" + remoteVersion);
+
+    if (localVersion === undefined || parseInt(remoteVersion) > parseInt(localVersion)) {
+        console.log("update required");
+        updateData();
+    }
+}
 
 function updateData() {
     $.ajax({
